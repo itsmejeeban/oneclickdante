a255138edde6b70e7cca8389e36dd9b9a6fc0abf
JEEBAN PANDIT <60313713+itsmejeeban@users.noreply.github.com>
2023-07-18 08:14:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@ sudo su
 apt update
 apt install dante-server
 danted -v
+cd /etc/init.d/
+rm -rf danted
+wget https://raw.githubusercontent.com/itsmejeeban/oneclickdante/main/danted
+chmod +x /etc/init.d/danted
+update-rc.d danted defaults
+cd /etc/
+rm -rf danted.conf
+wget https://raw.githubusercontent.com/itsmejeeban/oneclickdante/main/danted.conf
+systemctl start danted
